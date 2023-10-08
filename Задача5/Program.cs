56a56250815a28089deb43975067e4cdf0128c8b
IT <spanyukhin@list.ru>
2023-10-08 20:00:46 +0300

    задача5

---
+++ Задача5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Введите число n, которое формирует интервал значений от -n до n.
+Console.WriteLine("Введите число: ");
+int a = int.Parse(System.Console.ReadLine());
+int i = -a;
+
+while (i <= a ){
+    Console.WriteLine($" {i}");
+    i++;
+};
